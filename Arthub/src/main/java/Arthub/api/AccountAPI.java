86e9f448f2e17c009a9ed01653104b16c5bca04f
Đoàn Nam Sơn <namson1821@gmail.com>
2025-02-08 20:42:03 +0700

    Create Email Form With Token

--- Arthub/src/main/java/Arthub/api/AccountAPI.java
+++ Arthub/src/main/java/Arthub/api/AccountAPI.java
@@ -2,7 +2,6 @@ package Arthub.api;
 
 import Arthub.dto.AccountDTO;
 import Arthub.service.EmailTokenService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
