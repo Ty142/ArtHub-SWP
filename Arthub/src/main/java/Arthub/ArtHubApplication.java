887102ab2de5a535aac632e53008aaa4cc832499
Đoàn Nam Sơn <namson1821@gmail.com>
2025-02-08 16:01:21 +0700

    add

--- Arthub/src/main/java/Arthub/ArtHubApplication.java
+++ Arthub/src/main/java/Arthub/ArtHubApplication.java
@@ -3,7 +3,7 @@ package Arthub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "Arthub")
 public class ArtHubApplication {
 
     public static void main(String[] args) {
--- Arthub/src/main/java/Arthub/api/AccountAPI.java
+++ Arthub/src/main/java/Arthub/api/AccountAPI.java
@@ -1,5 +1,11 @@
 package Arthub.api;
 
+import Arthub.dto.AccountDTO;
+import Arthub.service.EmailTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import Arthub.entity.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +15,26 @@ import Arthub.service.AccountService;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/Account")
+
 public class AccountAPI {
 
+    private final EmailTokenService emailTokenService;
+
     @Autowired
-    AccountService accountService;
-
-
-    @GetMapping(value = "/api/Account")
-    public List<Account> getAccounts() {
-        return accountService.getAccounts();
+    public AccountAPI(EmailTokenService emailTokenService) {
+        this.emailTokenService = emailTokenService;
     }
 
+    @PostMapping("/send-token")
+    public ResponseEntity<String> sendTokenToEmail(@RequestBody AccountDTO accountDTO) {
+        String email = accountDTO.getEmail();
+
+        if (email == null || email.isEmpty()) {
+            return ResponseEntity.badRequest().body("Email cannot be null or empty.");
+        }
+
+        String token = emailTokenService.generateAndSendToken(email);
+        return ResponseEntity.ok(token);
+    }
 }
